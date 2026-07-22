70bd24eb0b512a5e2c5a26d21b65e40a0b5f3283
Erik Shaver <shaver.eriks@gmail.com>
2026-07-21 18:13:20 -0700

    Spotify stuff added

--- pomodoro_app/lib/config/env.dart
+++ pomodoro_app/lib/config/env.dart
@@ -3,8 +3,6 @@ class Env {
   static const supabaseAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3dWtweXZudXF1aWFvbmlhYWFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMTU1MjAsImV4cCI6MjA5OTg5MTUyMH0.9raqUTx-Z6OhH-STrFfzr32d2J-51UJf9VNUiBV2WN0';
 
-  // TODO: Replace with your Spotify Developer App Client ID
-  // Get yours at: https://developer.spotify.com/dashboard
-  static const spotifyClientId = 'YOUR_SPOTIFY_CLIENT_ID_HERE';
-  static const spotifyRedirectUri = 'http://localhost:8080';
+  static const spotifyClientId = 'e0d2fbe502cb4c0b968e27658650ea60';
+  static const spotifyRedirectUri = 'https://pomodoro.erikshaver.xyz';
 }
